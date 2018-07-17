fa2992a13539a29da019a9cb9806ea62969efefb
0918nobita <nobita.0918@gmail.com>
2018-07-17 18:30:33 +0900

    「加算の結果を変数aに代入する文」を表すオブジェクトを生成するようにした

--- Smilepp/src/net/zero918nobita/inverse/Main.java
+++ Smilepp/src/net/zero918nobita/inverse/Main.java
@@ -1,7 +1,14 @@
 package net.zero918nobita.inverse;
 
+import net.zero918nobita.smilepp.frontend.Addition;
+import net.zero918nobita.smilepp.frontend.AssignStm;
+import net.zero918nobita.smilepp.frontend.Builder;
+import net.zero918nobita.smilepp.frontend.Variable;
+
 public class Main {
   public static void main(String... args) {
-    System.out.println("Hello, world!");
+    Builder builder = new Builder();
+    builder.addStm(new AssignStm(new Variable("a"), new Addition()));
+    System.out.println(builder.toString());
   }
 }
