551ddf7dd47e5f58d316fd3ec1e05013626845be
0918nobita <nobita.0918@gmail.com>
2018-07-17 20:10:28 +0900

    Add constructor to Addition class

--- Smilepp/src/net/zero918nobita/smilepp/frontend/Addition.java
+++ Smilepp/src/net/zero918nobita/smilepp/frontend/Addition.java
@@ -1,4 +1,25 @@
 package net.zero918nobita.smilepp.frontend;
 
 public class Addition extends Expression {
+  private Expression expr1;
+  private Expression expr2;
+
+  public Addition() {}
+
+  public Addition(Expression expr1, Expression expr2) {
+    this.expr1 = expr1;
+    this.expr2 = expr2;
+  }
+
+  public void setExpr1(Expression expr) {
+    expr1 = expr;
+  }
+
+  public void setExpr2(Expression expr) {
+    expr2 = expr;
+  }
+
+  public String toString() {
+    return expr1 + " + " + expr2;
+  }
 }
