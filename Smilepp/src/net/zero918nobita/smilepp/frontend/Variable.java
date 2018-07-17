565a2d193038739d5be9893273988acbf3dc33a2
0918nobita <nobita.0918@gmail.com>
2018-07-17 20:11:26 +0900

    Overload the constructor of Variable

--- Smilepp/src/net/zero918nobita/smilepp/frontend/Variable.java
+++ Smilepp/src/net/zero918nobita/smilepp/frontend/Variable.java
@@ -3,7 +3,13 @@ package net.zero918nobita.smilepp.frontend;
 public class Variable extends Expression {
   private String name;
 
+  public Variable() {}
+
   public Variable(String name) {
+    this.name = name;
+  }
+
+  public void setName(String name) {
     this.name = name;
   }
 
