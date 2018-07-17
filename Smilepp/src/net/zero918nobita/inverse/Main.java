1fb9b95247144503d038cdc5054b714ff01f2f8f
0918nobita <nobita.0918@gmail.com>
2018-07-17 20:50:20 +0900

    Smile++ IRを出力するサンプルを追加した

--- Smilepp/src/net/zero918nobita/inverse/Main.java
+++ Smilepp/src/net/zero918nobita/inverse/Main.java
@@ -3,12 +3,17 @@ package net.zero918nobita.inverse;
 import net.zero918nobita.smilepp.frontend.Addition;
 import net.zero918nobita.smilepp.frontend.AssignStm;
 import net.zero918nobita.smilepp.frontend.Builder;
+import net.zero918nobita.smilepp.frontend.IntLiteral;
 import net.zero918nobita.smilepp.frontend.Variable;
 
 public class Main {
   public static void main(String... args) {
     Builder builder = new Builder();
-    builder.addStm(new AssignStm(new Variable("a"), new Addition()));
-    System.out.println(builder.toString());
+    Variable a = new Variable("a");
+    Variable b = new Variable("b");
+    builder.addStm(new AssignStm(a, new IntLiteral(2)));
+    builder.addLabel();
+    builder.addStm(new Addition(b, a, new IntLiteral(3)));
+    System.out.println(builder);
   }
 }
