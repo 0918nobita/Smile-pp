4d276fcd46d380c81c38b91c76303f01b6401dee
0918nobita <nobita.0918@gmail.com>
2018-07-19 17:24:44 +0900

    Add Token class

---
+++ Smilepp/src/net/zero918nobita/acinonyx/Token.java
@@ -0,0 +1,25 @@
+package net.zero918nobita.acinonyx;
+
+class Token {
+  private String name;
+  private int line;
+  private int letter;
+
+  Token(String name, int line, int letter) {
+    this.name = name;
+    this.line = line;
+    this.letter = letter;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof Token)) return false;
+    Token token = (Token) o;
+    return (token.letter == this.letter) && (token.line == this.line) && (token.name.equals(this.name));
+  }
+
+  @Override
+  public String toString() {
+    return "[\"" + name + "\", (" + line + ", " + letter + ")]";
+  }
+}
