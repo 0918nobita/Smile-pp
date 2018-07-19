b810049b3f6f60fe7f166d92bb40f653d3cdf6a3
0918nobita <nobita.0918@gmail.com>
2018-07-19 17:24:56 +0900

    字句解析のテストコードを追加した

--- Smilepp/src/net/zero918nobita/acinonyx/Main.java
+++ Smilepp/src/net/zero918nobita/acinonyx/Main.java
@@ -8,8 +8,27 @@ import net.zero918nobita.smilepp.frontend.IntLiteral;
 import net.zero918nobita.smilepp.frontend.Label;
 import net.zero918nobita.smilepp.frontend.Variable;
 
+import java.util.ArrayList;
+
 public class Main {
   public static void main(String... args) {
+    String src = "let a = 2;\na ++;\nconsole.log(a)\n";
+    System.out.println(src);
+
+    ArrayList<ArrayList<Token>> result = Lexer.lex(src);
+
+    for (int i_r = 0; i_r < result.size(); i_r++) {
+      System.out.print(i_r + 1 + ": ");
+
+      for (int i_t = 0; i_t < result.get(i_r).size(); i_t++) {
+        System.out.print(result.get(i_r).get(i_t));
+        if (i_t != result.get(i_r).size() - 1) System.out.print(", ");
+      }
+
+      System.out.println();
+    }
+
+    System.out.println();
     Builder builder = new Builder();
     Variable a = new Variable("a");
     Variable b = new Variable("b");
